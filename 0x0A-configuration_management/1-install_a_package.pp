ee2341dae694815d6a72c1c8c7db3dff62b4d235
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-22 19:43:04 +0100

    exec command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,3 @@
-#!/user/bin/pup
 #  install flask from pip3
 
 package { 'flask':
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# 2-execute_a_command.pp
+
+exec { 'kill_process_killmenow':
+  command     => 'pkill -f killmenow',
+  refreshonly => true,
+}
