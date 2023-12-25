e3552936573b222e7e258e8afd5f5383e636ab68
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-25 15:05:58 +0100

    Client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # make changes to our configuration file using puppet
 
-file_line { 'Turn off passwd auth':
+file_line { ' refuse to authenticate using a password':
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   ensure => present,
 }
 
-file_line { 'Declare identity file':
+file_line { 'use the private key':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
   ensure => present,
