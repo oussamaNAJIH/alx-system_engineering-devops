bca20bb4bebbc79d4eb250804c6272f85eb91a20
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-13 19:15:58 +0100

    Recurse it

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,9 +7,10 @@ import requests
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    params = {'limit': 10}
+    response = requests.get(url, params=params, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
