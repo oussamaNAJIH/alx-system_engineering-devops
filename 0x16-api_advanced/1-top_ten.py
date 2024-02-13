863832c082a332135cde0f03507e1403dbcaaa14
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-13 18:59:43 +0100

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+python file to print the titles of the first
+10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
