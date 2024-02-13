b08967d4a6399450376c8598af3d8b2f683ca0ed
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-13 19:23:27 +0100

    count it

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,6 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and
-    returns the number of subscribers for a given subreddit
-    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+python file to prints a sorted count of given keywords
+"""
+import requests
+
+
+def count_words(subreddit, word_list, after=None, counts=None):
+    if counts is None:
+        counts = {}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {'limit': 100, 'after': after}
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in word_list:
+                if word.lower() in title:
+                    counts[word.lower()] = counts.get(word.lower(), 0) + 1
+        after = data['data']['after']
+        if after is not None:
+            return count_words(subreddit, word_list, after, counts)
+        else:
+            sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+            for word, count in sorted_counts:
+                print(f"{word}: {count}")
+    else:
+        print(None)
