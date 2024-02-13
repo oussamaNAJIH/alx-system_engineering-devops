0cae78a6ee7415e72f86c2125a4770d3de152578
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-13 19:07:35 +0100

    how many

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,24 +5,17 @@ python file to returns the number of subscribers for a given subreddit
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and
-    returns the number of subscribers for a given subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
-        return 0
-
-
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print(number_of_subscribers(sys.argv[1]))
+    def number_of_subscribers(subreddit):
+        """
+        function that queries the Reddit API and
+        returns the number of subscribers for a given subreddit
+        """
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
