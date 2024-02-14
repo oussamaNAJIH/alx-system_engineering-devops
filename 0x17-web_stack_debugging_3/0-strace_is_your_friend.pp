d82c39208f56638e537922e684e0e4c5b22d8e2b
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-14 17:07:50 +0100

    error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,7 @@
 # Puppet manifest to fix Apache 500 error
 $file_path = '/var/www/html/wp-settings.php'
 
-file_line { 'replace_line':
-  path    => $file_path,
-  line    => '    define(\'DB_CHARSET\', \'utf8\');',
-  match   => '    define(\'DB_CHARSET\', \'utf-8\');',
-  ensure  => present,
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_path}",
+  path    => ['/bin','/usr/bin']
 }
